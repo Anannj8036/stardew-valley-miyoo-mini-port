c1bfaca153bb3eef5f92f87d6069cf88a36243c2
Producdevity <y.gherbi.dev@gmail.com>
2026-07-26 21:11:07 +0200

    build: shit, forgot the hleper

---
+++ scripts/common.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+fail() {
+    printf 'error: %s\n' "$*" >&2
+    exit 1
+}
+
+sha256_file() {
+    if command -v shasum >/dev/null 2>&1; then
+        shasum -a 256 "$1" | awk '{print $1}'
+    elif command -v sha256sum >/dev/null 2>&1; then
+        sha256sum "$1" | awk '{print $1}'
+    else
+        fail 'shasum or sha256sum is required'
+    fi
+}
